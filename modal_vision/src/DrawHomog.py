144bcf0469cc6b10a3c058d67ca7d4fa856189da
pierre-alexandre-masset <pierre-alexandre.masset@polytechnique.edu>
2016-12-16 14:33:21 +0100

    Add files via upload

--- modal_vision/src/DrawHomog.py
+++ modal_vision/src/DrawHomog.py
@@ -4,7 +4,14 @@ from matplotlib import pyplot as plt
 
 class DrawHomog(object):
     
-    def __init__(self):
-        im_finale = np.
+    def __init__(self, calcHomog):
+        [hmin, hmax, wmin, wmax] = calcHomog.path.corners
+        self.H = hmax-hmin
+        self.W = wmax-wmin
+        self.trans = -hmin, -wmin
+        
+        
+    def drawFinal():
+        im_finale = np.zeros((H,W), np.uint8)
     
         
