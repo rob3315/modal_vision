22482d557ecb8e5de6f2c902a3457673531fec1b
Remi ROBIN <remi.robin@polytechnique.edu>
2016-12-16 16:19:01 +0100

    upgrade drawhomog

--- modal_vision/src/DrawHomog.py
+++ modal_vision/src/DrawHomog.py
@@ -14,19 +14,15 @@ class DrawHomog(object):
         self.Mtrans = [[1,0,-wmin],[0,1,-hmin],[0,0,1]]
         
         
-    def drawFinal(self):
+    def drawFinal(self,lst_img):
         im_finale = np.zeros((self.W,self.H), np.uint8)
-        
-        img1 = cv2.imread('1.jpg',0)
-        img2 = cv2.imread('2.jpg',0)
-        img3 = cv2.imread('3.jpg',0)
-     
-        img1distorted = cv2.warpPerspective(img1, np.dot(self.Mtrans, self.calcHomog.path.listHomography[1-1]), (self.W,self.H))
-        img2distorted = cv2.warpPerspective(img2, np.dot(self.Mtrans, self.calcHomog.path.listHomography[2-1]), (self.W,self.H))
-        img3distorted = cv2.warpPerspective(img3, np.dot(self.Mtrans, self.calcHomog.path.listHomography[3-1]), (self.W,self.H))
-        
-        im_finale = img1distorted/3+img2distorted/3+img3distorted/3
-        
+        lst_img_distorted=map(lambda img :cv2.warpPerspective(img, np.dot(self.Mtrans, self.calcHomog.path.listHomography[1-1]), (self.W,self.H)))        
+        def f(x,y):
+            if x==0: return y
+            else : return (x+y)/2
+        for i in range(self.W):
+            for j in range(self.H):
+                im_finale[i][j]=reduce(f,lst_img_distorted)
         cv2.imshow("Image", im_finale)
         
         cv2.waitKey(0)
