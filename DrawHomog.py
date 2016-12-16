da843d6d26f53afd965988cea52a79bfc97f1836
pierre-alexandre-masset <pierre-alexandre.masset@polytechnique.edu>
2016-12-16 14:36:16 +0100

    Add files via upload

---
+++ DrawHomog.py
@@ -0,0 +1,19 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
+class DrawHomog(object):
+    
+    def __init__(self, calcHomog):
+        [hmin, hmax, wmin, wmax] = calcHomog.path.corners
+        self.H = hmax-hmin
+        self.W = wmax-wmin
+        self.trans = -hmin, -wmin
+        self.calcHomog = calcHomog
+        
+        
+    def drawFinal(self, ):
+        im_finale = np.zeros((self.H,self.W), np.uint8)
+        
+    
+        
